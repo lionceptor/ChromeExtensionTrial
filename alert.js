488cff40fdf60e8bf8c27a629cd4bf624b362597
Lyon Lee <lyonyjlee@gmail.com>
2018-03-06 14:53:30 -0600

    create for loops

--- alert.js
+++ alert.js
@@ -3,4 +3,10 @@ document.addEventListener('DOMContentLoaded',function()
     var buttonCheck = document.getElementById('languageCheck');
     var lang = "Language: \n";
     buttonCheck.addEventListener('click',function()
-  
+    {
+        chrome.tabs.detectLanguage(tab.id, function(dict)
+        {
+
+        });
+    });
+});
