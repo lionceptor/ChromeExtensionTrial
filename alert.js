dae5c31d3a4de569915d2d84b28a1323486757fd
Lyon Lee <lyonyjlee@gmail.com>
2018-03-06 14:53:43 -0600

    populate for loop

--- alert.js
+++ alert.js
@@ -6,7 +6,10 @@ document.addEventListener('DOMContentLoaded',function()
     {
         chrome.tabs.detectLanguage(tab.id, function(dict)
         {
-
+            for(var m = 0; m < dict.languages.length; m++)
+            {
+                lang = lang + dict.languages[m].language + " ";
+            }
         });
     });
 });
