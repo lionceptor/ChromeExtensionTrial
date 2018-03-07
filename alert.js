236d32944f66010c53918ee268c4b8990354ac68
Lyon Lee <lyonyjlee@gmail.com>
2018-03-06 19:37:28 -0600

    fix syntax and add comments

--- alert.js
+++ alert.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded',function()
 {
+    //links buttonCheck to clicking the language button
     var buttonCheck = document.getElementById("languageCheck");
     var lang = "Language: \n";
+
+    //alert of what language it is when button is pressed
     buttonCheck.addEventListener("click",function()
     {
         chrome.tabs.getSelected(null, function(tab)
         {
             chrome.tabs.detectLanguage(tab.id, function(dict)
             {
-                alert(dict);
+                lang += dict;
+                alert(lang);
             });
         });
     });
+
 });
